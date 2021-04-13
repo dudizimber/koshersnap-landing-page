5bdd4133a9bbcd301d8949c5799a3e5cf0166fc7
David Zimberknopf <dudi@vizo.dev>
2021-04-13 16:11:53 +0300

    first version

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="body">
+    <img class="logo" alt="Kosher Snap logo" src="./assets/hand.svg" />
+    <div class="text">
+      <img alt="Kosher" src="./assets/kosher.svg" />
+      <img alt="Snap" src="./assets/snap.svg" />
+    </div>
+    <h2>Coming soon...</h2>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  components: {},
 });
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #5127c7;
+  color: white;
+  font-family: 'Nunito', 'Roboto', sans-serif, Arial, Helvetica;
 }
+
+.body {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+
+.logo {
+  max-width: 500px;
+  min-width: 250px;
+  margin-bottom: 32px;
+}
+
+.text {
+  display: flex;
+  align-items: center;
+
+  :last-child {
+    margin-top: 31px;
+    margin-left: 8px;
+  }
+}
+
 </style>
